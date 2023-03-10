01894f0a40c5a8b1124d021a962d30aad52487e6
Bello Toybat <98482637+Tybello-123@users.noreply.github.com>
2023-03-11 00:03:46 +0100

    2nd commit

--- script.js
+++ script.js
@@ -3,40 +3,87 @@
 
 
 
-const passwordEl = document.getElementById('pass-el');
+const resultEl = document.getElementById('result-el');
 const copyBtn = document.getElementById('copy-btn');
-const genBtn = document.getElementById('submit');
+const generateEl = document.getElementById('submit');
 const passwordLength = document.getElementById('pass-len');
+const upperCaseEl = document.getElementById('uppercase');
+const lowerCaseEl = document.getElementById('lowercase');
+const numbersEl = document.getElementById('numbers');
+const symbolsEl = document.getElementById('symbols');
 
 
-let passwordArray = []
-let i;
 
-const checkUpperCase = () => {
+// add an event listener to the generator button
+generateEl.addEventListener( 'click', () => {
+    const length = +passwordLength.value;
+    const hasUpper = upperCaseEl.checked;
+    const hasLower = lowerCaseEl.checked;
+  
+    const hasNumbers = numbersEl.checked;
+    const hasSymbols = symbolsEl.checked;
+
+    resultEl.innerText = generatePassword(
+       
+        hasUpper,
+        hasLower,
+        hasNumbers,
+        hasSymbols,
+        length
+        );
+})
+
+// generate password function
+
+function generatePassword(upper, lower, number, symbol, length){
+ // 1. Init pw var
+ //2. Filter out unchecked types
+ // 3. loop over length call generator function for each type
+ // 4. add final pw to the pw var return
+
+  let generatedPassword = '';
+  const typesCount = upper + lower + number + symbol;
+
+  console.log('typesCount: ', typesCount);
+
+  const typesArr = [{lower}, {upper}, {number}, {symbol}].filter(
+    item => Object.values(item)[0]
+  );
+
+  console.log('typesArr: ', typesArr);
+
+
+}
+
+
+//Generator functions for lower case letters , upper case letters,number liter and symbols
+
+const genUpperCase = () => {
     
-   for(i = 65 ; i < 95 ; i++){
-      console.log(String.fromCharCode(i));
-   }
+    return String.fromCharCode(Math.floor(Math.random() * 26 + 96))
+   
 }
 
-const checkLowerCase = () => {
-    for (i = 97; i <= 122; i++) {
-        console.log(String.fromCharCode(i));
-    }
+const genLowerCase = () => {
+  return String.fromCharCode(Math.floor(Math.random()*26 + 65))
 }
 
-const checkSymbols = () => {
-    alert("checked");
-    console.log('working');
+const genSymbols = () => {
+    const symbols = '!@#$%^&*()*,-./[]{}+,?<>';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+
 }
 
-const checkNumbers = () => {
-    alert("checked");
-    console.log('working');
+const genNumbers = () => {
+   return String.fromCharCode(Math.floor(Math.random() * 10 + 48))
 }
 
 
-document.getElementById('cbox-2').addEventListener('click', checkLowerCase);
-document.getElementById('cbox-3').addEventListener('click', checkNumbers);
-document.getElementById('cbox-4').addEventListener('click', checkSymbols);
-document.getElementById('cbox-1').addEventListener('click', checkUpperCase);
+//store the generator functions in the objects
+
+const randonFunc = {
+    upper : genUpperCase,
+    lower : genLowerCase,
+    number : genNumbers,
+    symbol : genSymbols
+};
